8b077685f7e6667b7767f9a1f7922c4af7422496
Macharia Marigi <macharia.marigi@andela.com>
2018-06-28 21:24:26 +0300

    Parsing request paths

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const http = require('http')
+const url = require('url')
 
+// server responding to requests
 const server = http.createServer((req, res) => {
+
+  // Get the url and parse it
+  const parsedUrl = url.parse(req.url, true)
+
+  // get trimmed path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/, '')
+
+  // send response
   res.end('Hello World\n')
+  console.log(`Request received on path ${trimmedPath}`)
 })
 
 // start the server and have it listening on prt 3000

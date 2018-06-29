7aba30ac2e47532cfcde5d2d8dc97bdc67a9e9ea
Macharia Marigi <macharia.marigi@andela.com>
2018-06-29 10:29:57 +0300

    Parsing payloads

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
 
 // server responding to requests
 const server = http.createServer((req, res) => {
@@ -20,10 +21,21 @@ const server = http.createServer((req, res) => {
   // get the headers as an object
   const headers = req.headers
 
-  // send response
-  res.end('Hello World\n')
-  console.log('Headers: ', headers)
-  console.log(`${method} request received on path ${trimmedPath} with the following query strings `, queryStringObject)
+  // get the payload if there's any
+  const decoder = new StringDecoder('utf-8')
+  let buffer = '';
+  req.on('data', (data) => buffer += decoder.write(data))
+
+  req.on('end', () => {
+    buffer += decoder.end()
+    
+    // send response
+    res.end('Hello World\n')
+    console.log('Headers: ', headers)
+    console.log(`${method} request received on path ${trimmedPath} with the following query strings `, queryStringObject)
+    console.log('Payload: ', buffer)
+  })
+
 })
 
 // start the server and have it listening on prt 3000
